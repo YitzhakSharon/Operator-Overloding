502341117d936e12704dfa6067e5bac1a4ed44ac
YitzhakSharon <yitzhak.sharon7@gmail.com>
2018-04-22 22:38:48 +0300

    yit

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,6 +1,7 @@
 #include "CircularInt.hpp"
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
     // CircularInt:: CircularInt(int a, int b):begin(a),end(b),current(a){
@@ -124,9 +125,14 @@ CircularInt& operator- ( CircularInt& a, CircularInt& b ){// we use twice at the
 }
 CircularInt& operator/ ( CircularInt& other, int num ){
     CircularInt &other1 (other);
-    if(num!=0)
+    if(num!=0){
         other1.current=range(other1,other1.current/num);
-    return other1;
+        return other1;
+}
+    else{
+        string s ="There is no number x in {1,12} such that";
+        throw s;
+    }
 
 }
  CircularInt& operator/= (  CircularInt& a, int num){
@@ -143,31 +149,31 @@ istream& operator >> (istream& is,CircularInt& ci){
     ci.current=range(ci,ci.current);
     return is;
 }
-// int main(){
-// 	CircularInt hour {1, 12};                 // <hour is an integer between 1 and 12, like an hour on the clock>
-// 	cout << hour << endl;                     // 1
-// 	hour += 4;  
-//     cout << hour << endl;         // 5
-// 	(hour += 2)++; 
-//      cout << hour << endl;     // 8
-// 	hour += 18;   
-//     cout << hour << endl;       // 2   (18 hours after 8)
-// 	cout << -hour << endl;                    // 10  (2 hours before midnight)
-// 	hour = 1 - hour; 
-//     cout << hour << endl;    // 11  (2 hours before 1)
-// 	cout << hour+hour << endl;                // 10 (11 hours after 11)
-// 	hour *= 2;   
-//     cout << hour << endl;        // 10 (11*2 = 11+11)
-// 	cout<< hour/2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
+int main(){
+	CircularInt hour {1, 12};                 // <hour is an integer between 1 and 12, like an hour on the clock>
+	cout << hour << endl;                     // 1
+	hour += 4;  
+    cout << hour << endl;         // 5
+	(hour += 2)++; 
+     cout << hour << endl;     // 8
+	hour += 18;   
+    cout << hour << endl;       // 2   (18 hours after 8)
+	cout << -hour << endl;                    // 10  (2 hours before midnight)
+	hour = 1 - hour; 
+    cout << hour << endl;    // 11  (2 hours before 1)
+	cout << hour+hour << endl;                // 10 (11 hours after 11)
+	hour *= 2;   
+    cout << hour << endl;        // 10 (11*2 = 11+11)
+	cout<< hour/2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
 
-// 	try {
-// 		cout<< hour/3;
-// 	} catch (const string& message) {
-// 		cout << message << endl;     // "There is no number x in {1,12} such that x*3=10"
-// 	}
+	try {
+		cout<< hour/3;
+	} catch (const string& message) {
+		cout << message << endl;     // "There is no number x in {1,12} such that x*3=10"
+	}
 
-// 	// RIDDLES (not for submission): 
-// 	//  * when is there exactly one answer to a/b?
-// //  * when are there two or more answers to a/b?
-// 	//	* when is there no answer to a/b?
-// }
+	// RIDDLES (not for submission): 
+	//  * when is there exactly one answer to a/b?
+//  * when are there two or more answers to a/b?
+	//	* when is there no answer to a/b?
+}
